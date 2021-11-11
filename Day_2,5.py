4583b2383dc29889332c39c990a09df0c39f17d1
Nicol Gotzova <nicol.gotzova@gmail.com>
2021-11-11 09:52:00 +0100

    převod na int + seřazení

--- Day_2,5.py
+++ Day_2,5.py
@@ -1,5 +1,5 @@
 
-f = open("/Users/nicolgotzova/Documents/Advent of Code/Day_2.txt", encoding="utf-8")
+f = open("/Users/nicolgotzova/Documents/Advent_of_Code/Day_2.txt", encoding="utf-8")
 complete_paper = f.read()
 f.close()
 lines = complete_paper.split('\n')
@@ -10,9 +10,9 @@ for line in lines:
     counter = counter + 1
     values_list = line.split("x")
     #sorted_values_list = sorted(values_list)   #nevím proč, ale hodnoty mi to neseřadí od nejnižší 
-    #sorted (vyska, sirka, delka)               #zase řve, že potřebuje jen jeden argument místo 3
-    #min (values_list)                          #mi vyhodí jen nejnižší hodnotu. potřebuju poslední dvě hodnoty, co potřebuju
-    sirka = int(sorted_values_list [0])
+    sorted(values_list)               #zase řve, že potřebuje jen jeden argument místo 3
+    #min(values_list)                          #mi vyhodí jen nejnižší hodnotu. potřebuju ale poslední dvě hodnoty
+    """sirka = int(sorted_values_list [0])
     vyska = int(sorted_values_list [1])
     delka = int(sorted_values_list [2])
     
@@ -20,12 +20,12 @@ for line in lines:
     present = (sirka+sirka+vyska+vyska)
     extra_paper = (sirka*vyska*delka)
     total = present + extra_paper
+    """
     
-    
-    celkovy_objem = celkovy_objem + total
+    #celkovy_objem = celkovy_objem + total
     """if counter == 5:
         break"""
      
-print (f"Celkovy objem je: {celkovy_objem}")
+#print (f"Celkovy objem je: {celkovy_objem}")
 
-
+print (values_list)
